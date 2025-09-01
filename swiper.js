5df87dfb8ed4b9ce3ec443c82ce767e2f1cd2e52
up81bgclub <up81battlegroundclub@gmail.com>
2025-09-01 22:02:05 +0530

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+  });
